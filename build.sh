157f6b5500e92402bd56a92d705c989e5e0900cc
Gregor Aisch <contact@vis4.net>
2013-03-01 13:09:05 +0100

    removed duplicate license header

--- build.sh
+++ build.sh
@@ -8,9 +8,7 @@ cat src/core.coffee > tmp
 cat src/core/*.coffee >> tmp
 cat src/modules/*.coffee >> tmp
 cat src/modules/symbols/*.coffee >> tmp
-cat tmp | coffee -sp > tmp2
-cat src/license tmp2 > kartograph.js
-rm tmp tmp2
+cat tmp | coffee -sp > kartograph.js
 uglifyjs kartograph.js > kartograph.min.js
 mv kartograph*.js dist
 echo "build complete"
